f90a429b9099cf95a5a2644d65cebf2f9e603e31
Juanes Mejia <juanesteban8@gmail.com>
2021-02-10 18:46:44 -0500

    add test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 18:39:16 2021
+
+@author: Juanes Mejía
+"""
+
+## Esto es una prueba de codigo en Python
+
+
+import sys 
+import pandas as pd
+import numpy as np
